1029b50659cf8717b3ca404641b7ba017783388e
chalapathisudheer <chalapathisudheer@gmail.com>
2018-04-17 12:43:08 +0530

    17/04/2018

--- src/test/java/mulesoft/training/com/HelloMavenTest.java
+++ src/test/java/mulesoft/training/com/HelloMavenTest.java
@@ -9,7 +9,7 @@ public class HelloMavenTest extends FunctionalTestCase {
 
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
-        runFlowAndExpect("mavenFlow", "Hello This is my first Maven Project");
+        runFlowAndExpect("mavenFlow", "Hello: This is my first Maven Project and building through Jenkins");
     }
     
     @Override
